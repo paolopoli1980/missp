1235f48aa5eafe018b666610a1798dcd033d6751
paolopoli1980 <paolopoli1980@gmail.com>
2017-07-30 19:48:42 +0200

    Add files via upload

---
+++ v2in2017/ex4/casualscript.py
@@ -0,0 +1,18 @@
+
+import random
+f1=open('pos.aut','w')
+
+for i in range(20):
+	
+	x=random.uniform(-1,1)
+	y=random.uniform(-1,1)
+	z=random.uniform(-1,1)
+	f1.write(str(x)+str('\n'))
+	f1.write(str(y)+str('\n'))
+  
+ 
+
+f1.write('*\n')
+
+
+f1.close()
